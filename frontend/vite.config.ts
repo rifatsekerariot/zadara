6f2fd3fdcaa072879cf8d15912e451d996582f51
RifatSekerAriot <rifat.seker@ariot.com.tr>
2026-01-17 01:19:23 +0300

    Zadara-VMware Wrapper Projesi: Ilk Yukleme

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Proxy to our Node.js Middleware
+        target: 'http://localhost:3009', // Proxy to our Node.js Middleware
         changeOrigin: true,
         secure: false,
       },
